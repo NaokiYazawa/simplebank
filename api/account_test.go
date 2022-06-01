e024cc37f52f33203858ba473a5b46bdf9edda29
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-06-02 08:46:57 +0900

    TestGetAccountAPI

---
+++ api/account_test.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/NaokiYazawa/simplebank/db/mock"
+	db "github.com/NaokiYazawa/simplebank/db/sqlc"
+	"github.com/NaokiYazawa/simplebank/util"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	// build stubs
+	store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(account, nil)
+
+	// start test server and sed request
+	server := NewServer(store)
+	recoder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	server.router.ServeHTTP(recoder, request)
+	// check response
+	require.Equal(t, http.StatusOK, recoder.Code)
+	requireBodyMatchAccount(t, recoder.Body, account)
+}
+
+func randomAccount() db.Accounts {
+	return db.Accounts{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+}
+
+func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Accounts) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount db.Accounts
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, account, gotAccount)
+}
