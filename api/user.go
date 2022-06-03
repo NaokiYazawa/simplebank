697a49e4a4ae65034eec8bc6c252e5c87840dbbd
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-06-03 18:05:01 +0900

    Hash password in Go with Bcrypt

---
+++ api/user.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/NaokiYazawa/simplebank/db/sqlc"
+	"github.com/NaokiYazawa/simplebank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"` // 特殊文字を含めないようにする
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"` // email のバリデーションを追加
+}
+
+type userResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+} // ハッシュ化されたパスワードをレスポンスに含めないようにする
+
+func newUserResponse(user db.Users) userResponse {
+	return userResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.Email,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	rsp := newUserResponse(user)
+	ctx.JSON(http.StatusOK, rsp)
+}
